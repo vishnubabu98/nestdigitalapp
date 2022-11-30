1352d40166e1d9082322a9d6ac565195f1c49d5e
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-30 23:29:38 +0530

    add emplloyye navbar

---
+++ src/app/empnavbar/empnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empnavbar',
+  templateUrl: './empnavbar.component.html',
+  styleUrls: ['./empnavbar.component.css']
+})
+export class EmpnavbarComponent {
+
+}
